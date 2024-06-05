c7966050f157b7304d3875459a0cc7e95317c784
theProtagonist09 <mrinal10305@gmail.com>
2024-06-05 18:39:30 +0530

    type any message here

--- C++/arrays/findNumThatAppearOnceWhenOthersAppearTwice/findNumThatAppearOnce.cpp
+++ C++/arrays/findNumThatAppearOnceWhenOthersAppearTwice/findNumThatAppearOnce.cpp
@@ -3,8 +3,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int singleElement(arr){
+    int n=arr.size();
+
+    for(int i=0;j<n;i++){
+        int num=arr[i];  //selecting a particular element one by one
+        int cnt=0;
+
+        for(int j=0;j<n;j++){
+            if(num==arr[j]);
+            cnt++;
+        }
+        if(cnt==1){
+            return num;
+        }
+    }
+    return -1;   //this will only be executed,if arr doesn't contain a single element
+}
+
 int main(){
     int n=5;
-    int arr[n]={4,1,2,1,2};
+    vector <int> arr={4,1,2,1,2};
 
 }
