96996779c2510564cdbbdff0762a0c25aafe66ad
theProtagonist09 <mrinal10305@gmail.com>
2024-06-05 18:54:07 +0530

    changes

--- C++/arrays/findNumThatAppearOnceWhenOthersAppearTwice/findNumThatAppearOnce.cpp
+++ C++/arrays/findNumThatAppearOnceWhenOthersAppearTwice/findNumThatAppearOnce.cpp
@@ -3,16 +3,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int singleElement(arr){
+int singleElement(vector <int> &arr){ //&arr ensure we pass by reference. i.e Any modifications to arr inside the function will affect the original vector.
     int n=arr.size();
 
-    for(int i=0;j<n;i++){
+    for(int i=0;i<n;i++){
         int num=arr[i];  //selecting a particular element one by one
         int cnt=0;
 
         for(int j=0;j<n;j++){
-            if(num==arr[j]);
+            if(num==arr[j]){
             cnt++;
+            }
         }
         if(cnt==1){
             return num;
@@ -22,7 +23,7 @@ int singleElement(arr){
 }
 
 int main(){
-    int n=5;
+
     vector <int> arr={4,1,2,1,2};
     int ans= singleElement(arr);
     cout<<"Single Element is "<<ans<<endl;
