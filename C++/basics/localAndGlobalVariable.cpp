5a1c41bda1e303e965962ce6a7c4638d3e0b7303
theProtagonist09 <mrinal10305@gmail.com>
2024-06-06 19:04:01 +0530

    hello

---
+++ C++/basics/localAndGlobalVariable.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    for(int i=0;i<5;i++){
+        int sum=2;
+    }
+    
+    cout<<sum;  //error, as 'sum' was not declared in this scope
+    return 0;
+}
